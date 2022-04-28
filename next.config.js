36e95e3fd5127a2576356d1f2fad865ff9124396
Vincent Voyer <vincent@codeagain.com>
2022-04-28 12:16:24 +0200

    lets go

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/redirect",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
